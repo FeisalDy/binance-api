33d7e6f7b8bd89d626a3d30572f196fc546de5ba
feisaldy <feisaldy26@gmail.com>
2023-07-19 12:34:50 +0700

    pre loading

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -21,15 +21,26 @@ const ProfilePage = () => {
         if (session?.user.id) fetchPosts()
     }, [])
 
-    if (!posts.length) {
-        return <p>Loading...</p>
-    }
-
     const handleEdit = post => {
         router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = async post => {}
+    const handleDelete = async post => {
+        const hasConfirmed = confirm('Are you sure you want to delete this?')
+
+        if (!hasConfirmed) return
+
+        try {
+            await fetch(`/api/prompt/${post._id.toString()}`, {
+                method: 'DELETE'
+            })
+
+            const filteredPosts = posts.filter(p => p._id !== post._id)
+            setPosts(filteredPosts)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <Profile
